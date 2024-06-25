9dfc8718dadc656710e3dabdd56b19d5e49d0060
Gui Silva <adrianon10@gmail.com>
2024-06-25 03:51:18 -0300

    feat: add knex config

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,13 @@
 import Fastify from 'fastify'
+import { knex } from './database/database'
 
 const PORT = 3333 || process.env.PORT
 const app = Fastify()
+
+app.get('/', async () => {
+  const tables = await knex('sqlite_schema').select('*')
+  return tables
+})
 
 app
   .listen({
