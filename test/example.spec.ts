1410b3d84d64fa238a797401598072b08c268502
Gui Silva <adrianon10@gmail.com>
2024-06-29 03:43:44 -0300

    chore: add vitest

---
+++ test/example.spec.ts
@@ -0,0 +1,9 @@
+import { describe, expect, it } from 'vitest'
+
+describe('', () => {
+  it('should be able to user create a transaction', async () => {
+    const responseStatusHttp = 201
+
+    expect(responseStatusHttp).toEqual(201)
+  })
+})
